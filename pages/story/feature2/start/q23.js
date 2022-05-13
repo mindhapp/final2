37ae028c6d5c5f001f68205440877fd25cf94c85
mindhapp <thishxp@gmail.com>
2022-05-13 22:21:26 +0700

    Create q23.js

---
+++ pages/story/feature2/start/q23.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { store } from "../../../../components/store/store";
+import Link from "next/link";
+import { observer } from "mobx-react-lite";
+
+const Q23 = observer(() => {
+  return (
+    <div>
+      <center>
+        <div className=" border border-solid py-10">
+          {" "}
+          <h1>หลังจากคุณเล่น Story ของเราคุณจะกลับไปหาคนที่คุณรักแล้ว ...</h1>
+          <br />
+          <Link href="/story/feature2/start/q3">
+            <button onClick={() =>
+              // store.addCharacter("S")
+              store.addCharacter({ question: 23, character: 'S', index: 1, questionnumber:23 })
+            }>
+              {" "}
+              <i className="fa-solid fa-circle" />{" "}
+              วางแผนใช้ชีวิตกับคนที่คุณรักให้คุ้มค่ามากที่สุด
+            </button>
+          </Link>
+          <br />
+          <br />
+          <Link href="/story/feature2/start/q3">
+            <button onClick={() =>
+              // store.addCharacter("N")
+              store.addCharacter({ question: 23, character: 'N', index: 1 , questionnumber:23})
+            }>
+              {" "}
+              <i className="fa-solid fa-circle" />{" "}
+              จะใช้ชีวิตที่เหลืออยู่กับคนที่คุณรักอย่างมีความสุขที่สุดไปเรื่อยๆ
+            </button>
+          </Link>
+        </div>
+      </center>
+    </div>
+  );
+});
+
+export default Q23;
