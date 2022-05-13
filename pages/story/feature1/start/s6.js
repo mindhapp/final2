4083ece5fb01b705dd503e4dc2a3e1c8acc7549e
mindhapp <thishxp@gmail.com>
2022-05-13 22:21:01 +0700

    Create s6.js

---
+++ pages/story/feature1/start/s6.js
@@ -0,0 +1,49 @@
+import React, { useRef, useContext } from "react";
+import { useRouter } from "next/router";
+
+import { Context } from '../../../../context'
+import { updateAnswer } from '../../../../services/updateAnswer'
+
+
+function S6() {
+  const router = useRouter();
+  const [authState] = useContext(Context)
+
+  const inputRef = useRef(null)
+
+  const submit = async (e) => {
+    e.preventDefault();
+
+    // call firestore to set value
+    await updateAnswer('feature1', authState.uid, {
+      s6: inputRef.current.value,
+    })
+
+    router.push("/story/feature1/start/s7");
+  };
+  return (
+    <div>
+      <center>
+        <form onSubmit={submit}>
+          <div className=" cursor-pointer pt-24">
+            <label>สิ่งแรกที่คุณทำตอนตื่นนอนคืออะไร</label>
+            <br />
+            <br />
+            <input
+              type="text"
+              required
+              className=" bg-black rounded-md py-4 px-6 text-white"
+              placeholder="พิมพ์ข้อความ..."
+              ref={inputRef}
+            />
+            <br /> <br />
+            <button type="submit">{`>> ถัดไป`}</button>
+          </div>
+        </form>
+        <br />
+      </center>
+    </div>
+  );
+}
+
+export default S6;
